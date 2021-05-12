fb3d4072329d50c311e5853c8c92ef20d58eb540
sreenathuser <56663730+sreenathuser@users.noreply.github.com>
2021-05-12 11:47:54 +0530

    29th line to azurerm_template_deployment

--- main.tf
+++ main.tf
@@ -26,10 +26,10 @@ resource "azurerm_resource_group" "RG-for-logicapp" {
     }
 }
 
-resource "azurerm_resource_group_template_deployment" "my_logic_app" {
+resource "azurerm_template_deployment" "my_logic_app" {
     name = "DSGlogicapp"
     resource_group_name = azurerm_resource_group.RG-for-logicapp.name
-    template_content = templatefile("/home/runner/work/demo_logic_app/demo_logic_app/template.json", {})
-    parameters_content = templatefile("/home/runner/work/demo_logic_app/demo_logic_app/parameters.json", {})
+    template_body = file("/home/runner/work/demo_logic_app/demo_logic_app/template.json", {})
+    parameters_body = file("/home/runner/work/demo_logic_app/demo_logic_app/parameters.json", {})
     deployment_mode = "Incremental" 
 }
